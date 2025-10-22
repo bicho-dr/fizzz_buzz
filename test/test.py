9c921d34cccdfd2ab834bc09786db3fbafbe34c4
bachirdridi <    GitHub>
2025-10-22 13:01:06 +0100

    the scande cases failing

--- test/test.py
+++ test/test.py
@@ -2,3 +2,6 @@ def test()-> None:
     from src.fizz_buzz import fizz_buzz
     fizz_buzz()
 
+def test_fizzbuzz_returns_number_for_non_multiples():
+    result = fizzbuzz(1)
+    assert result == "1"
